77090f1d04229d5b744af29dd70460670dc01f6e
Tomoaki Yagishita <tyagishi@gmail.com>
2020-10-06 12:23:27 +0900

    add public

--- Sources/SwiftUIDebugUtil/SwiftUIDebugUtil.swift
+++ Sources/SwiftUIDebugUtil/SwiftUIDebugUtil.swift
@@ -9,40 +9,40 @@ import Foundation
 import SwiftUI
 
 extension View {
-    func debugAction(_ closure: () -> Void) -> Self {
-        #if DEBUG
-        closure()
-        #endif
-
-        return self
-    }
+  public func debugAction(_ closure: () -> Void) -> Self {
+    #if DEBUG
+    closure()
+    #endif
+    
+    return self
+  }
 }
 extension View {
-    func debugPrint(_ value: Any) -> Self {
-        debugAction { print(value) }
-    }
-}
-
-extension View {
-    func debugModifier<T: View>(_ modifier: (Self) -> T) -> some View {
-        #if DEBUG
-        return modifier(self)
-        #else
-        return self
-        #endif
-    }
+  public func debugPrint(_ value: Any) -> Self {
+    debugAction { print(value) }
+  }
 }
 
 extension View {
-    func debugBorder(_ color: Color = .red, width: CGFloat = 1) -> some View {
-        debugModifier {
-            $0.border(color, width: width)
-        }
-    }
+  public func debugModifier<T: View>(_ modifier: (Self) -> T) -> some View {
+    #if DEBUG
+    return modifier(self)
+    #else
+    return self
+    #endif
+  }
+}
 
-    func debugBackground(_ color: Color = .red) -> some View {
-        debugModifier {
-            $0.background(color)
-        }
+extension View {
+  public func debugBorder(_ color: Color = .red, width: CGFloat = 1) -> some View {
+    debugModifier {
+      $0.border(color, width: width)
     }
+  }
+  
+  public func debugBackground(_ color: Color = .red) -> some View {
+    debugModifier {
+      $0.background(color)
+    }
+  }
 }
