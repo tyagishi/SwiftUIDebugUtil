8d90cefa4306a6ac5ef1d264d1c38fab728ac1a8
Tomoaki Yagishita <tyagishi@gmail.com>
2021-04-14 13:49:23 +0900

    ok to show clock, slideshow

--- Sources/SwiftUIDebugUtil/SwiftUIDebugUtil.swift
+++ Sources/SwiftUIDebugUtil/SwiftUIDebugUtil.swift
@@ -46,3 +46,43 @@ extension View {
     }
   }
 }
+
+// util for preview
+// Usage:
+// struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+//            .landscape()
+//        ContentView()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone 8 Plus"))
+//            .landscape()
+//        ContentView() // iPhone 12 Pro
+//            .landscape()
+//    }
+// }
+struct LandscapeModifier: ViewModifier {
+    let height = UIScreen.main.bounds.width
+    let width = UIScreen.main.bounds.height
+    
+    var isPad: Bool { // 1
+        return height >= 768
+    }
+
+    var isRegularWidth: Bool { // 2
+        return height >= 414
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .previewLayout(.fixed(width: width, height: height))
+            .environment(\.horizontalSizeClass, isRegularWidth ? .regular: .compact) // 5
+            .environment(\.verticalSizeClass, isPad ? .regular: .compact) // 6
+    }
+}
+
+extension View {
+    public func landscape() -> some View {
+        self.modifier(LandscapeModifier())
+    }
+}
